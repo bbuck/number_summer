50d3f6c44875786b3979a5da9df2e0dde544e115
Brandon Buck <lordizuriel@gmail.com>
2015-05-18 00:30:14 -0500

    Step 2 complete.

--- number_summer.go
+++ number_summer.go
@@ -2,8 +2,31 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	if len(os.Args) == 1 {
+		fmt.Printf("USAGE: %s num[, num[, num[, ...]]]\n", os.Args[0])
+
+		return
+	}
+
+	numStrings := os.Args[1:]
+	nums := make([]int, len(numStrings))
+	for i, ns := range numStrings {
+		intVal, err := strconv.Atoi(ns)
+		if err != nil {
+			fmt.Printf("Invalid number %q was given!\n", ns)
+		}
+		nums[i] = intVal
+	}
+
+	sum := 0
+	for _, n := range nums {
+		sum += n
+	}
+
+	fmt.Printf("The sum of the given values was %d\n", sum)
 }
